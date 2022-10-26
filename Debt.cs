11ab74740c6618b1f0421b4c353faed1ff93a466
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-10-26 14:03:56 +0300

    Add project files.

---
+++ Debt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debt
+{
+    public class Debt1
+    {
+        private double _intialBalance;
+        private double _initialInterestRate;
+        public Debt1(double intialBalance, double initialInterestRate)
+        {
+            _intialBalance = intialBalance;
+            _initialInterestRate = initialInterestRate;
+        }
+        public void WaitOneWaitOneYear()
+        {
+            _intialBalance = _intialBalance * _initialInterestRate;
+        }
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Balance: {_intialBalance} InterestRate: {_initialInterestRate}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Debt;
+
+    Debt1 mortgage = new Debt1(120000.0, 1.01);
+    mortgage.PrintBalance();
+
+    mortgage.WaitOneWaitOneYear();
+    mortgage.PrintBalance();
+
+    // Wait 20 years
+    int years = 0;
+    while (years < 20)
+    {
+        mortgage.WaitOneWaitOneYear();
+        years = years + 1;
+    }
+
+    mortgage.PrintBalance();
+
